2d8c66d36080c14737b550ccad7d85f6ae6f7ce3
“polevych” <polevich2801@gmail.com>
2023-04-02 15:45:20 +0200

    feat: USDT

--- test/vending-machine.test.ts
+++ test/vending-machine.test.ts
@@ -10,6 +10,7 @@ enum EVENT_TYPES {
 describe("VendingMachine", function () {
   let vendingMachine: Contract,
     USDC: Contract,
+    USDT: Contract,
     token: Contract,
     TOKEN_TO_USDC_RATE: number,
     MINIMUM_PURCHASE_AMOUNT: number,
@@ -25,9 +26,12 @@ describe("VendingMachine", function () {
       .connect(owner)
       .deploy("ERC20", "ERC20")) as Token;
     USDC = (await tokenFactory.connect(owner).deploy("USDC", "USDC")) as Token;
+    USDT = (await tokenFactory.connect(owner).deploy("USDT", "USDT")) as Token;
 
     await USDC.connect(owner).mint(await user1.getAddress(), 15 * 10 ** 6);
     await USDC.connect(owner).mint(await user2.getAddress(), 15 * 10 ** 6);
+    await USDT.connect(owner).mint(await user1.getAddress(), 15 * 10 ** 6);
+    await USDT.connect(owner).mint(await user2.getAddress(), 15 * 10 ** 6);
 
     const vendingMacineFactory = await ethers.getContractFactory(
       "VendingMachine"
@@ -36,16 +40,21 @@ describe("VendingMachine", function () {
     TOKEN_TO_USDC_RATE = 10;
     MINIMUM_PURCHASE_AMOUNT = 10;
 
-    vendingMachine = (await vendingMacineFactory.connect(owner).deploy(
-      USDC.address,
-      TOKEN_TO_USDC_RATE,
-      token.address,
-      MINIMUM_PURCHASE_AMOUNT,
-      "0x572dDec9087154dC5dfBB1546Bb62713147e0Ab0", // Polygon Mumbai (USDC / USD) | decimals - 8
-      "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada" // Polygon Mumbai (MATIC / USD) | decimals - 8
-    )) as VendingMachine;
+    vendingMachine = (await vendingMacineFactory
+      .connect(owner)
+      .deploy(
+        USDC.address,
+        USDT.address,
+        token.address,
+        TOKEN_TO_USDC_RATE,
+        MINIMUM_PURCHASE_AMOUNT,
+        "0x572dDec9087154dC5dfBB1546Bb62713147e0Ab0",
+        "0x92C09849638959196E976289418e5973CC96d645",
+        "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada"
+      )) as VendingMachine;
 
     await USDC.connect(owner).mint(vendingMachine.address, 10 * 10 ** 6);
+    await USDT.connect(owner).mint(vendingMachine.address, 10 * 10 ** 6);
 
     await USDC.connect(user1).increaseAllowance(
       vendingMachine.address,
@@ -53,6 +62,16 @@ describe("VendingMachine", function () {
     );
 
     await USDC.connect(user2).increaseAllowance(
+      vendingMachine.address,
+      1 * 10 ** 6
+    );
+
+    await USDT.connect(user1).increaseAllowance(
+      vendingMachine.address,
+      5 * 10 ** 6
+    );
+
+    await USDT.connect(user2).increaseAllowance(
       vendingMachine.address,
       1 * 10 ** 6
     );
@@ -69,54 +88,56 @@ describe("VendingMachine", function () {
       .increaseAllowance(vendingMachine.address, ethers.utils.parseEther("10"));
   });
 
-  it("should convert tokens to USDC", async function () {
+  it("should convert tokens to stable coins (USDC/USDT)", async function () {
     expect(
-      await vendingMachine.convertToUSDC(ethers.utils.parseEther("2"))
+      await vendingMachine.convertToStable(ethers.utils.parseEther("2"))
     ).to.be.equal(
       (Number(ethers.utils.parseEther("2")) * TOKEN_TO_USDC_RATE) / 10 ** 14
     );
   });
 
-  it("should convert tokens from USDC", async function () {
-    expect(await vendingMachine.convertFromUSDC(10000000)).to.be.equal(
+  it("should convert tokens from stable coins (USDC/USDT)", async function () {
+    expect(await vendingMachine.convertFromStable(10000000)).to.be.equal(
       BigInt((10000000 * 100) / TOKEN_TO_USDC_RATE) * BigInt(10 ** 12)
     );
   });
 
-  describe("purchaseForUSDC", function () {
+  describe("purchase core", function () {
     it("should fail if the purchase amount is less than the minimum purchase amount", async function () {
       const purchaseAmount = MINIMUM_PURCHASE_AMOUNT - 1;
       await expect(
-        vendingMachine.purchaseForUSDC(purchaseAmount)
+        vendingMachine.purchaseForStable(purchaseAmount, USDC.address)
       ).to.be.revertedWith(
         "Minimum purchase amount is greater than input amount"
       );
     });
 
-    it("should fail if the Veding machine does not have enough TOKEN to cover the purchase", async function () {
+    it("should fail if the Veding machine does not have enough ERC20 to cover the purchase", async function () {
       await expect(
         vendingMachine
           .connect(user1)
-          .purchaseForUSDC(ethers.utils.parseEther("60000"))
+          .purchaseForStable(ethers.utils.parseEther("60000"), USDC.address)
       ).to.be.revertedWith("Insufficient funds in ERC20");
     });
 
-    it("should fail if caller does not have enough USDC to cover the purchase", async function () {
+    it("should fail if caller does not have enough stable coins to cover the purchase", async function () {
       await expect(
         vendingMachine
           .connect(user1)
-          .purchaseForUSDC(ethers.utils.parseEther("15000"))
-      ).to.be.revertedWith("Insufficient funds in USDC");
+          .purchaseForStable(ethers.utils.parseEther("15000"), USDC.address)
+      ).to.be.revertedWith("Insufficient funds in USDT/USDC");
     });
 
-    it("should fail if caller does not give enough allowance inside USDC for the Vending contract", async function () {
+    it("should fail if caller does not give enough allowance inside stable coins for the Vending contract", async function () {
       await expect(
         vendingMachine
           .connect(user2)
-          .purchaseForUSDC(ethers.utils.parseEther("130"))
-      ).to.be.revertedWith("Insufficient allowance in USDC");
+          .purchaseForStable(ethers.utils.parseEther("130"), USDC.address)
+      ).to.be.revertedWith("Insufficient allowance in USDT/USDC");
     });
+  });
 
+  describe("purchaseForUSDC", function () {
     it("should purchase tokens for USDC and transfer them to the caller", async function () {
       const tokenAmount = 15;
       const userERC20BalanceBefore = await token.balanceOf(
@@ -128,7 +149,7 @@ describe("VendingMachine", function () {
 
       const tx = await vendingMachine
         .connect(user1)
-        .purchaseForUSDC(tokenAmount);
+        .purchaseForStable(tokenAmount, USDC.address);
 
       const events = (await tx.wait()).events;
       const transferEvents = events
@@ -143,7 +164,7 @@ describe("VendingMachine", function () {
 
       expect(transferEvents[0].from).to.be.equal(await user1.getAddress());
       expect(transferEvents[0].amount).to.be.equal(
-        await vendingMachine.convertToUSDC(tokenAmount)
+        await vendingMachine.convertToStable(tokenAmount)
       );
       expect(transferEvents[0].to).to.be.equal(vendingMachine.address);
 
@@ -157,25 +178,78 @@ describe("VendingMachine", function () {
 
       expect(await USDC.balanceOf(vendingMachine.address)).to.be.equal(
         vendingUSDCBalanceBefore.add(
-          await vendingMachine.convertToUSDC(tokenAmount)
+          await vendingMachine.convertToStable(tokenAmount)
         )
       );
     });
   });
 
-  describe("withdraw", function () {
+  describe("purchaseForUSDT", function () {
+    it("should purchase tokens for USDT and transfer them to the caller", async function () {
+      const tokenAmount = 15;
+      const userERC20BalanceBefore = await token.balanceOf(
+        await user1.getAddress()
+      );
+      const vendingUSDTBalanceBefore = await USDT.balanceOf(
+        vendingMachine.address
+      );
+
+      const tx = await vendingMachine
+        .connect(user1)
+        .purchaseForStable(tokenAmount, USDT.address);
+
+      const events = (await tx.wait()).events;
+      const transferEvents = events
+        .filter(
+          (event: Event) =>
+            event.event === EVENT_TYPES.TRANSFER && event.args !== undefined
+        )
+        .map((event: Event) => event.args);
+
+      expect(transferEvents && transferEvents[0] && transferEvents[1]).to.not.be
+        .undefined;
+
+      expect(transferEvents[0].from).to.be.equal(await user1.getAddress());
+      expect(transferEvents[0].amount).to.be.equal(
+        await vendingMachine.convertToStable(tokenAmount)
+      );
+      expect(transferEvents[0].to).to.be.equal(vendingMachine.address);
+
+      expect(transferEvents[1].from).to.be.equal(vendingMachine.address);
+      expect(transferEvents[1].amount).to.be.equal(tokenAmount);
+      expect(transferEvents[1].to).to.be.equal(await user1.getAddress());
+
+      expect(await token.balanceOf(await user1.getAddress())).to.be.equal(
+        userERC20BalanceBefore.add(tokenAmount)
+      );
+
+      expect(await USDC.balanceOf(vendingMachine.address)).to.be.equal(
+        vendingUSDTBalanceBefore.add(
+          await vendingMachine.convertToStable(tokenAmount)
+        )
+      );
+    });
+  });
+
+  describe("withdraw core", function () {
     it("should fail if caller does not have enough ERC20 to cover the purchase", async function () {
       await expect(
-        vendingMachine.connect(user1).withdraw(ethers.utils.parseEther("18000"))
-      ).to.be.revertedWith("Insufficient funds in ERC20");
+        vendingMachine
+          .connect(user1)
+          .withdrawStable(ethers.utils.parseEther("18000"), USDC.address)
+      ).to.be.revertedWith("Insufficient funds in the Vending contract");
     });
 
     it("should fail if caller does not give enough allowance inside ERC20 for the Vending contract", async function () {
       await expect(
-        vendingMachine.connect(user2).withdraw(ethers.utils.parseEther("12"))
+        vendingMachine
+          .connect(user2)
+          .withdrawStable(ethers.utils.parseEther("12"), USDC.address)
       ).to.be.revertedWith("Insufficient allowance in ERC20");
     });
+  });
 
+  describe("withdraw USDC", function () {
     it("should withdraw funds from ERC20 to USDC", async function () {
       const tokenAmount = 10;
       await vendingMachine.connect(user2).purchaseForUSDC(tokenAmount);
@@ -187,11 +261,37 @@ describe("VendingMachine", function () {
         vendingMachine.address
       );
 
-      await vendingMachine.connect(user2).withdraw(tokenAmount);
+      await vendingMachine.connect(user2).withdrawUSDC(tokenAmount);
 
       expect(await USDC.balanceOf(await user2.getAddress())).to.be.equal(
         userUSDCBalanceBefore.add(
-          await vendingMachine.convertToUSDC(tokenAmount)
+          await vendingMachine.convertToStable(tokenAmount)
+        )
+      );
+
+      expect(await token.balanceOf(vendingMachine.address)).to.be.equal(
+        vendingERC20BalanceBefore.add(tokenAmount)
+      );
+    });
+  });
+
+  describe("withdraw USDT", function () {
+    it("should withdraw funds from ERC20 to USDT", async function () {
+      const tokenAmount = 10;
+      await vendingMachine.connect(user2).purchaseForUSDT(tokenAmount);
+
+      const userUSDCBalanceBefore = await USDT.balanceOf(
+        await user2.getAddress()
+      );
+      const vendingERC20BalanceBefore = await token.balanceOf(
+        vendingMachine.address
+      );
+
+      await vendingMachine.connect(user2).withdrawUSDT(tokenAmount);
+
+      expect(await USDT.balanceOf(await user2.getAddress())).to.be.equal(
+        userUSDCBalanceBefore.add(
+          await vendingMachine.convertToStable(tokenAmount)
         )
       );
 
